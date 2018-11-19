56afe9cb14fb830fbffbb591a140e65f95febe8b
Georgy Schukin <georgy.schukin@gmail.com>
2018-11-20 02:29:52 +0700

    Update Springer patterns

--- src/crawlers/springer_crawler.py
+++ src/crawlers/springer_crawler.py
@@ -4,5 +4,5 @@ from crawlers.email_crawler import EmailCrawler
 class SprigerCrawler(EmailCrawler):
     def __init__(self):
         super().__init__()
-        self.set_patterns([r'springer\.com.*/chapter/', r'springer\.com.*/article/', r'springer\.com.*/book/'])
+        self.set_patterns([r'springer\.com/chapter/', r'springer\.com/article/', r'springer\.com/book/'])
 
